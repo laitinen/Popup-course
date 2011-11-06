0011238d08ed98a886e4a261a753ec1f7b0f3606
Pekka Mikkola <pjmikkol@cs.helsinki.fi>
2011-11-06 16:28:32 +0200

    Trying to hunt down bugs.

--- triangulation/triangleToPic.cpp
+++ triangulation/triangleToPic.cpp
@@ -4,18 +4,21 @@
 #include <utility>
 #include <cmath>
 #include <vector>
+#include <cassert>
+
+typedef unsigned char byte;
 
 class Picture {
  public:
   Picture(int w, int h) : m_w(w), m_h(h) {
-    m_pic = new int[m_w*m_h*3];
+    m_pic = new byte[m_w*m_h*3];
     for(size_t i = 0; i < m_w*m_h*3; ++i) {
-      m_pic[i] = 0;
+      m_pic[i] = 255;
     }
   }
 
   Picture(const Picture& other) : m_w(other.m_w), m_h(other.m_h) {
-    m_pic = new int[m_w*m_h*3];
+    m_pic = new byte[m_w*m_h*3];
     std::copy(other.m_pic, other.m_pic + m_w*m_h*3, m_pic);
   }
 
@@ -30,20 +33,62 @@ class Picture {
 
   void paintColor(int x, int y, int *color) {
     if( x < 0 || x >= m_w || y < 0 || y >= m_h ) return;
-    int *pixel = (m_pic + 3*m_w*y + 3*x);
-    for(size_t i = 0; i < 3; ++i)
-      pixel[i] = (int) (((255 - color[3])/255.0)*pixel[i]);
+    byte *pixel = (m_pic + 3*m_w*y + 3*x);
+    for(size_t i = 0; i < 3; ++i) {
+      double alpha = color[3]/255.0;
+      pixel[i] = (byte) ((1 - alpha)*pixel[i] + alpha*color[i]);
+    }
   }
   
+  void writeToTGA(const char* fname) {
+    std::ofstream out(fname);
+    if(!out) {
+      std::cerr << "Couldn't open file " << fname << std::endl;
+      return;
+    }
+    byte header[] = {0, 0,
+                     2, //image format == uncompressed RGB
+                     0, 0,
+                     0, 0,
+                     0,
+                     0, 0, // x-coordinate of bottom left corner
+                     0, 0, // y-coordinate of bottom left corner
+                     m_w & 0x00FF, (m_w & 0xFF00) >> 8, // width
+                     m_h & 0x00FF, (m_h & 0xFF00) >> 8, // height
+                     24, //bits per pixel
+                     0};
+    for(size_t i = 0; i < sizeof(header); ++i) {
+      out.put(header[i]);
+    }
+    for(size_t i = 0; i < m_w*m_h*3; i += 3) {
+      out.put(m_pic[i + 2]);
+      out.put(m_pic[i + 1]);
+      out.put(m_pic[i]);
+    }
+  }
+
  private:
   int m_w, m_h;
-  int *m_pic;
+  byte *m_pic;
 
 };
 
 struct ivec2 {
   int x, y;
 };
+
+bool operator==(const ivec2& v1, const ivec2& v2) {
+  return v1.x == v2.x && v1.y == v2.y;
+}
+
+int dot(const ivec2& v1, const ivec2& v2) {
+  return v1.x*v2.x + v1.y*v2.y;
+}
+
+ivec2 operator-(const ivec2& v1, const ivec2& v2) {
+  ivec2 v = {v1.x - v2.x, v1.y - v2.y};
+  return v;
+}
 
 struct Edge {
  public:
@@ -67,6 +112,47 @@ struct Edge {
 
 };
 
+int scanLine(Picture& pic, Edge& edge, int *color, std::vector<int>& lineEnds, bool rightSide, int i) {
+  if(edge.v1.y == edge.v2.y) {
+    if(rightSide && edge.v1.x > edge.v2.x) std::swap(edge.v1, edge.v2);
+    else if(!rightSide && edge.v1.x < edge.v2.x) std::swap(edge.v1, edge.v2);
+  }
+
+  int dx = abs(edge.v1.x - edge.v2.x), dy = edge.height()-1;
+  int sx = (edge.v1.x < edge.v2.x)?1:-1;
+  int err = dx - dy;
+  int x1 = edge.v1.x, y1 = edge.v1.y, x2 = edge.v2.x, y2 = edge.v2.y;
+
+  for(;;) {
+    if(rightSide) {
+      for(int x = x1; x > lineEnds[i]; --x) pic.paintColor(x, y1, color);
+    } else {
+      for(int x = x1; x < lineEnds[i]; ++x) pic.paintColor(x, y1, color);
+    }
+    lineEnds.at(i) = x1;
+    if(x1 == x2 && y1 >= y2) break;
+    int e2 = 2*err;
+    if(e2 > -dy) {
+      err -= dy;
+      x1 += sx;
+    }
+    if (e2 < dx) {
+      err += dx;
+      ++y1;
+      ++i;
+    }
+  }
+
+  return i;
+}
+
+double cosAngle(const ivec2& v1, const ivec2& v2) {
+  double n1 = sqrt(dot(v1,v1)), n2 = sqrt(dot(v2,v2));
+  std::cout << "cosAngle of (" << v1.x << ", " << v1.y << ") and ("
+            << v2.x << ", " << v2.y << ") is " << dot(v1,v2)/(n1*n2) << std::endl;
+  return dot(v1,v2)/(n1*n2);
+}
+
 
 void rasterizeTriangle(Picture& pic, ivec2* vertices, int *color) {
   Edge edges[3];
@@ -78,13 +164,24 @@ void rasterizeTriangle(Picture& pic, ivec2* vertices, int *color) {
   size_t offset = edges[2].v2.y;
   std::vector<int> lineEndpoints;
   lineEndpoints.resize(height);
+  std::fill(lineEndpoints.begin(), lineEndpoints.end(), -2);
 
   // Now apply Bresenham's line rasterization algorithm for lines!
-  // First the longest line, store also the endpoint for scanlines
-  int dx = abs(edges[2].v1.x - edges[2].v2.x);
+  // First the heightest line is rasterized
+  // Decide whether the remaining point is on the left or the right side of the
+  // longest edge by using the properties of cross-product
+  ivec2 remPoint = (edges[0].v1 == edges[2].v1 || edges[0].v1 == edges[2].v2)?edges[0].v2:edges[0].v1;
+  ivec2 longestVec = edges[2].v2 - edges[2].v1;
+  ivec2 remVec = remPoint - edges[2].v1;
+  bool right = (longestVec.x*remVec.y - longestVec.y*remVec.x)  < 0;
+
+  if(right) std::cout << "right" << std::endl;
+  else std::cout << "left" << std::endl;
+
+  int dx = abs(edges[2].v1.x - edges[2].v2.x), dy = height-1;
   int sx = (edges[2].v1.x < edges[2].v2.x)?1:-1;
   int i = 0;
-  int err = dx - height;
+  int err = dx - dy;
   int x1 = edges[2].v1.x, y1 = edges[2].v1.y, x2 = edges[2].v2.x, y2 = edges[2].v2.y;
 
 
@@ -95,15 +192,25 @@ void rasterizeTriangle(Picture& pic, ivec2* vertices, int *color) {
     if(e2 > -dy) {
       err -= dy;
       x1 += sx;
+      if(right && sx > 0) lineEndpoints[i] = x1;
+      else if(!right && sx < 0) lineEndpoints[i] = x1;
     }
     if (e2 < dx) {
       err += dx;
+      if(lineEndpoints[i] == -2) lineEndpoints[i] = x1;
+      ++i;
       ++y1;
     }
   }
-  
+  assert(i == height-1);
+  if(edges[1].v1.y < edges[0].v1.y) std::swap(edges[1], edges[0]);
+  i = 0;
+  i = scanLine(pic, edges[0], color, lineEndpoints, right, i);
+  scanLine(pic, edges[1], color, lineEndpoints, right, i);
 
 }
+
+
 
 
 int main(int argc, char **argv) {
@@ -133,5 +240,7 @@ int main(int argc, char **argv) {
     }
     rasterizeTriangle(pic, V, rgba);
   }
+
+  pic.writeToTGA(argv[2]);
 
 }
